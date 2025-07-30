2ecb350ba73787fb0391300db7ec859504516f7a
Banana-chen <dashcamp057@gmail.com>
2025-07-30 18:14:55 +0800

    feat: add Pchome topic to enum

--- src/crawler_kit/modules/general/enums/topic.py
+++ src/crawler_kit/modules/general/enums/topic.py
@@ -4,6 +4,7 @@ from os import getenv
 
 class Topic(str, Enum):
     Test = "test"
+    Pchome = "pchome"
 
     def __str__(self):
         return f"projects/{getenv('PROJECT_ID')}/topics/{self.value}"
