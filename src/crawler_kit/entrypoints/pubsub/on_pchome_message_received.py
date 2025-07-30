9b97d7013ea19dac35a005e9ac1c665c5ec6be2b
Banana-chen <dashcamp057@gmail.com>
2025-07-30 19:09:33 +0800

    code formatted

--- src/crawler_kit/entrypoints/pubsub/on_pchome_message_received.py
+++ src/crawler_kit/entrypoints/pubsub/on_pchome_message_received.py
@@ -6,6 +6,7 @@ from firebase_functions.pubsub_fn import (
 from crawler_kit.utils.google_cloud.run_job import run_job
 from crawler_kit.modules.general.enums.topic import Topic
 
+
 @on_message_published(topic=str(Topic.Pchome))
 def handle_run_job_on_pchome_message_received(
     event: CloudEvent[MessagePublishedData],
--- src/crawler_kit/modules/greet/presentation/cli/handlers/handle_hello.py
+++ src/crawler_kit/modules/greet/presentation/cli/handlers/handle_hello.py
@@ -2,6 +2,7 @@ from click.core import Context
 from crawler_kit.utils.google_cloud.publish_message import publish_message
 from crawler_kit.modules.general.enums.topic import Topic
 
+
 def handle_hello(context: Context):
     topic = Topic.Test
     payload = dict(message="hello, world")
